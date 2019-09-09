82374fd4d5d489881dfa53ab8df96654c11ccd15
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-09-09 02:48:18 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,27 @@
+require 'pry'
 class Customer
-  
+  attr_accessor :name, :age
+  @@all=[]
+
+  def initialize (name,age)
+    @name=name
+    @age=age
+    @@all<<self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal (waiter,total,tip=0)
+    Meal.new(waiter,self,total,tip)
+  end
+
+  def meals
+    Meal.all.select {|x| x.customer==self}
+  end
+
+  def waiters
+    meals.map {|y| y.waiter}
+  end
 end
